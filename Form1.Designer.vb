88e32467c0d9d93708a70bb9d9ecb5501d583845
angel <angel@ANGELA2004>
2024-04-22 10:36:24 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grd_timetable = New UJGrid.UJGrid()
+        Me.SuspendLayout()
+        '
+        'grd_timetable
+        '
+        Me.grd_timetable.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.grd_timetable.Cols = 8
+        Me.grd_timetable.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.grd_timetable.FixedCols = 1
+        Me.grd_timetable.FixedRows = 1
+        Me.grd_timetable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.grd_timetable.Location = New System.Drawing.Point(4, 2)
+        Me.grd_timetable.Margin = New System.Windows.Forms.Padding(4)
+        Me.grd_timetable.Name = "grd_timetable"
+        Me.grd_timetable.Padding = New System.Windows.Forms.Padding(4)
+        Me.grd_timetable.Rows = 32
+        Me.grd_timetable.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grd_timetable.Size = New System.Drawing.Size(1223, 695)
+        Me.grd_timetable.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1329, 730)
+        Me.Controls.Add(Me.grd_timetable)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grd_timetable As UJGrid.UJGrid
+End Class
---
+++ Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+
+    Private Sub study_timetable(ByVal r As Integer, ByVal c As Integer, ByVal t As String)
+        grd_timetable.Row = r
+        grd_timetable.Col = c
+        grd_timetable.Text = t
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        study_timetable(0, 0, "Time")
+        study_timetable(0, 1, "Monday")
+        study_timetable(0, 2, "Tuesday")
+        study_timetable(0, 3, "Wednesday")
+        study_timetable(0, 4, "Thursday")
+        study_timetable(0, 5, "Friday")
+        study_timetable(0, 6, "Saturday")
+        study_timetable(0, 7, "Sunday")
+
+        study_timetable(2, 0, "08:00-09:00")
+        study_timetable(4, 0, "09:00-10:00")
+        study_timetable(6, 0, "10:00-11:00")
+        study_timetable(8, 0, "11:00-12:00")
+        study_timetable(10, 0, "12:00-13:00")
+        study_timetable(12, 0, "13:00-14:00")
+        study_timetable(14, 0, "14:00-15:00")
+        study_timetable(16, 0, "15:00-16:00")
+        study_timetable(18, 0, "16:00-17:00")
+        study_timetable(20, 0, "17:00-18:00")
+        study_timetable(22, 0, "18:00-19:00")
+        study_timetable(24, 0, "19:00-20:00")
+        study_timetable(26, 0, "20:00-21:00")
+        study_timetable(28, 0, "21:00-22:00")
+        study_timetable(30, 0, "22:00-23:00")
+
+        study_timetable(2, 1, "MATHS")
+        study_timetable(4, 1, "LFS")
+        study_timetable(8, 1, "INFOMATICS")
+        study_timetable(22, 1, "MATHS")
+        study_timetable(24, 1, "MATHS")
+        study_timetable(28, 1, "LFS")
+        study_timetable(30, 1, "INFOMATICS")
+
+
+        study_timetable(2, 2, "LFS")
+        study_timetable(4, 2, "INFOMATICS")
+        study_timetable(6, 2, "INFOMATICS")
+        study_timetable(8, 2, "MATHS")
+        study_timetable(22, 2, "MATHS")
+        study_timetable(30, 2, "INFOMATICS")
+
+
+        study_timetable(10, 3, "LFS")
+        study_timetable(18, 3, "INFOMATICS")
+        study_timetable(20, 3, "INFOMATICS")
+        study_timetable(24, 3, "MATHS")
+        study_timetable(26, 3, "MATHS")
+        study_timetable(30, 3, "INFOMATICS")
+
+        study_timetable(8, 4, "MATHS")
+        study_timetable(10, 4, "MATHS")
+        study_timetable(22, 4, "LFS")
+        study_timetable(26, 4, "INFOMATICS")
+
+        study_timetable(8, 5, "MATHS")
+        study_timetable(10, 5, "MATHS")
+        study_timetable(14, 5, "INFOMATICS")
+        study_timetable(26, 5, "LFS")
+
+        study_timetable(4, 6, "INFOMATIC")
+        study_timetable(6, 6, "INFOMATICS")
+        study_timetable(8, 6, "INFOMATICS")
+        study_timetable(14, 6, "MATHS")
+        study_timetable(16, 6, "MATHS")
+        study_timetable(18, 6, "MATHS")
+        study_timetable(26, 6, "LFS")
+
+        study_timetable(4, 7, "INFOMATICS")
+        study_timetable(6, 7, "INFOMATICS")
+        study_timetable(8, 7, "INFOMATICS")
+        study_timetable(12, 7, "LFS")
+        study_timetable(20, 7, "MATHS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
